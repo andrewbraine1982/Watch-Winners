c84357addb03f1fd5b2f47e16d0458a2ea332d89
andrewbraine1982 <andrewbraine@gmail.com>
2026-06-26 21:14:39 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,58 +1,26 @@
-type NavbarProps = {
-  brand?: string;
-};
-
-export default function Navbar({
-  brand = "WATCH WINNERS",
-}: NavbarProps) {
+export default function Navbar() {
   return (
     <header className="ww-navbar">
       <div className="ww-container">
-
         <a href="/" className="ww-logo">
-
-          <div className="ww-logo-mark">
-            W
-          </div>
-
-          <div className="ww-logo-text">
-            <span>WATCH</span>
-            <span>WINNERS</span>
-          </div>
-
+          <img
+            src="/logo.png"
+            alt="Watch Winners"
+            className="ww-logo-image"
+          />
         </a>
 
         <nav className="ww-nav-links">
-
-          <a href="#competitions">
-            Competitions
-          </a>
-
-          <a href="#how">
-            How it Works
-          </a>
-
-          <a href="#winners">
-            Winners
-          </a>
-
-          <a href="#trust">
-            Trust
-          </a>
-
-          <a href="#faq">
-            FAQ
-          </a>
-
+          <a href="#competitions">Competitions</a>
+          <a href="#how">How it Works</a>
+          <a href="#winners">Winners</a>
+          <a href="#trust">Trust</a>
+          <a href="#faq">FAQ</a>
         </nav>
 
-        <a
-          href="#competitions"
-          className="ww-enter-button"
-        >
+        <a href="#competitions" className="ww-enter-button">
           Enter Now
         </a>
-
       </div>
     </header>
   );
