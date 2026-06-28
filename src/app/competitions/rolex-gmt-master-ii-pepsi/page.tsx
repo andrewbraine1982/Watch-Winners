128947f20a45ecd80d785f9bf4303020c7013bf7
andrewbraine1982 <andrewbraine@gmail.com>
2026-06-28 15:07:04 +0200

    Update page.tsx

--- src/app/competitions/rolex-gmt-master-ii-pepsi/page.tsx
+++ src/app/competitions/rolex-gmt-master-ii-pepsi/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { useState } from "react";
 export default function CompetitionPage() {
+  const [selectedEntries, setSelectedEntries] = useState(5);
   return (
     <main className="competition-page">
 
